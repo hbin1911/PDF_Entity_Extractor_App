a39263e7cff400ae2c6ca2242a42c5223158c82a
Rajas <rajas.mulik@gmail.com>
2026-05-21 13:21:50 +0530

    Updated the app.py code to add the Highlighted entity view in UI

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import streamlit as st
 import tempfile
 
 from utils.pdf_extractor import extract_text_from_pdf
+from utils.ner_engine import extract_entities
 
 
-# Page title
+# Streamlit page title
 st.title("📄 AI-Powered PDF Entity Extractor")
 
 
@@ -15,10 +16,9 @@ uploaded_file = st.file_uploader(
 )
 
 
-# Process uploaded file
 if uploaded_file is not None:
 
-    # Save uploaded PDF temporarily
+    # Save uploaded file temporarily
     with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
 
         temp_file.write(uploaded_file.read())
@@ -28,7 +28,10 @@ if uploaded_file is not None:
     # Extract text
     extracted_text = extract_text_from_pdf(temp_path)
 
-    # Display success message
+    # Extract entities
+    entities = extract_entities(extracted_text)
+
+    # Success message
     st.success("PDF processed successfully!")
 
     # Show extracted text
@@ -37,5 +40,40 @@ if uploaded_file is not None:
     st.text_area(
         "Raw Text",
         extracted_text,
-        height=300
+        height=250
     )
+
+    # Show entities
+    st.subheader("🎯 Extracted Entities")
+
+    # Color mapping
+    colors = {
+        "PER": "#FF4B4B",
+        "ORG": "#1E90FF",
+        "LOC": "#32CD32",
+        "DATE": "#FFA500"
+    }
+
+    # Display highlighted entities
+    for entity in entities:
+
+        label = entity["label"]
+        word = entity["entity"]
+
+        color = colors.get(label, "#808080")
+
+        st.markdown(
+            f"""
+            <span style="
+                background-color:{color};
+                padding:6px;
+                border-radius:6px;
+                color:white;
+                margin-right:5px;
+                display:inline-block;
+            ">
+                {word} ({label})
+            </span>
+            """,
+            unsafe_allow_html=True
+        )
